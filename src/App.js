c662bdabb4219c77e96a10fb04cd04c1dd0f36d3
Zachary Wunder <zach.wunder@gmail.com>
2018-12-16 23:05:59 -0800

    update one

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from "react-router-dom";
+import { IndexRoute, Route } from "react-router-dom";
 
 import Home from "./pages/home";
 import Execution from "./pages/home";
@@ -10,7 +10,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <Route path="/" exact component={Home} />
+                <IndexRoute component={Home} />
                 <Route path="/execution" component={Execution} />
                 <Route path="/algorithmic" component={Algorithmic} />
                 <Route path="/roadmap" component={Roadmap} />
